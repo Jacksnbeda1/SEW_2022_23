47e0d497128037ee02f651a910519739b2e8e999
Jakob Führer <jakob.fuehrer@josephinum.at>
2022-12-01 09:00:27 +0100

    Test

---
+++ SEW4/1.1Vollmond/Program.cs
@@ -0,0 +1,29 @@
+﻿using _1._1Vollmond;
+
+
+internal class Program
+{
+    private static void Main(string[] args, MoonPhases moonPhases)
+    {
+        DateTime dt = DateTime.Now;
+        MoonPhases mp = MoonPhases.Newmoon;
+
+
+        while (true)
+        {
+            Moon.GetMoonPhase(dt, ref mp);
+
+            MoonPhases MoonPhases;
+            if (moonPhases == MoonPhases.Fullmoon)
+            {
+                Console.WriteLine(dt);
+            }
+            else
+            {
+                dt.AddDays(1);
+            }
+        }
+    }
+}
+
+
