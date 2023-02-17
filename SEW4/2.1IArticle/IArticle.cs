4e42fc03c4910466793ccec40e5947629d47c30a
Jakob Führer <jakob.fuehrer@josephinum.at>
2023-02-17 16:36:38 +0100

    Test fertig

---
+++ SEW4/2.1IArticle/IArticle.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2._1IArticle
+{
+    internal interface IArticle
+    {
+        public double Price { get; set; }
+        public string Description { get; set; }
+    }
+
+}
