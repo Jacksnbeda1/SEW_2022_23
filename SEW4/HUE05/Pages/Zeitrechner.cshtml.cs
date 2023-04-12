b44ca22c7539d6290a3da91f6aa95f721d44b75f
Jakob Jehle <jakob.jehle@josephinum.at>
2023-04-12 19:15:09 +0200

    HUE05 noch nicht fertig

---
+++ SEW4/HUE05/Pages/Zeitrechner.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace HUE05.Pages
+{
+    public class ZeitrechnerModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
