89dc95ae7f63a838205a86961e9af921c14c1db8
Jakob Jehle <jakob.jehle@josephinum.at>
2023-04-12 22:02:58 +0200

    HUE05 fertig

--- SEW4/HUE05/Pages/Zeitrechner.cshtml.cs
+++ SEW4/HUE05/Pages/Zeitrechner.cshtml.cs
@@ -5,8 +5,56 @@ namespace HUE05.Pages
 {
     public class ZeitrechnerModel : PageModel
     {
-        public void OnGet()
+        public string? Erg { get; set; }
+        public decimal HmAuf { get; set; }
+
+        public decimal HmAb { get; set; }
+
+        public decimal KmHo { get; set; }
+
+        public decimal HoDi { get; set; }
+
+        public decimal hours { get; set; }
+        public decimal minutes { get; set; }
+
+        [HttpPost]
+        public IActionResult OnPostRechner(decimal HmAuf, decimal HmAb, decimal KmHo)
         {
+            this.HmAuf = HmAuf / 300;
+            this.HmAb = HmAb/500;
+            this.KmHo = KmHo/4;
+            this.HoDi = this.HmAuf + this.HmAb;
+
+            if (HoDi < this.KmHo)
+            {
+                HoDi = this.HoDi / 2;
+            }
+            else
+            {
+                KmHo = this.KmHo / 2;
+            }
+            hours = HoDi + KmHo;
+
+            minutes = (hours - Math.Truncate(hours))*60;
+
+            if (minutes > 45)
+            {
+                minutes = 0;
+                hours = hours++;
+            }else if (minutes > 30)
+            {
+                minutes = 45;
+            }else if(minutes > 15)
+            {
+                minutes = 30;
+            }else if (minutes > 0)
+            {
+                minutes = 15;
+            }
+
+            Erg = $"{Math.Truncate(hours)} Stunden und {minutes} Minuten";
+
+            return Page();
         }
     }
 }
